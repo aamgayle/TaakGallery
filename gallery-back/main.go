3855610335902e82283303131a08d002f7d458ed
taakleton <azmatch.gayle@gmail.com>
2022-01-29 21:01:15 -0500

    Created test api for twitter keys

--- gallery-back/main.go
+++ gallery-back/main.go
@@ -6,26 +6,36 @@ import (
 	"log"
 	"net/http"
 
-	"gopkg.in/yaml.v3"
+	"gopkg.in/yaml.v2"
 )
 
 type config struct {
-	Key string `yaml: "keys"`
+	Key string `yaml:"twitter"`
 }
 
-func (c *config) getKeys() *config {
-	yamlFile, err := ioutil.ReadFile("../keys/keys.yml")
+func (c *config) getKeys() (*config, error) {
+	yamlFile, err := ioutil.ReadFile("keys/keys.yml")
+
 	if err != nil {
-		log.Printf("Issue getting yaml file #%v ", err)
+		log.Printf("There's an error here: %v", err)
 	}
+
 	err = yaml.Unmarshal(yamlFile, c)
-	return config
+	return c, err
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+	twitterConfig := new(config)
+	twitKey, err := twitterConfig.getKeys()
+	var key = twitKey.Key
+	if err != nil {
+		log.Printf("Issue getting yaml file #%v ", err)
+		return
+	}
+
 	fmt.Fprint(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
-	fmt.Printf("Here is the key: %s", config.Key)
+	fmt.Printf("Here is the key: %s", key)
 }
 
 func handleRequests() {
